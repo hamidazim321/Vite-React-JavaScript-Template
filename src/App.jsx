0f3a600f8d737afc75e7023938fb02f7e4c6f91c
Hamid Azim <hamidazim321@gmail.com>
2024-03-21 14:34:32 +0500

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
-
 function App() {
   return (
-    <div>
-      New App
+    <div className="text-3xl w-screen h-screen flex flex-col items-center justify-center gap-5">
+      <h1>New App with:</h1>
+      <ul>
+        <li>React</li>
+        <li>Tailwind</li>
+        <li>Redux</li>
+      </ul>
     </div>
   )
 }
