a87c68b35061f6a06b84190c938c23d83beb6b9d
Hamid Azim <hamidazim321@gmail.com>
2024-03-21 15:06:53 +0500

    Add Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
+import Intro from "./components/Intro"
+
 function App() {
   return (
-    <div className="text-3xl w-screen h-screen flex flex-col items-center justify-center gap-5">
-      <h1>New App with:</h1>
-      <ul>
-        <li>React</li>
-        <li>Tailwind</li>
-        <li>Redux</li>
-      </ul>
-    </div>
+    <>
+      <Intro />
+    </>
   )
 }
 
---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div className="text-3xl flex flex-col items-center justify-center gap-5">
+      <h1>New App with:</h1>
+      <ul>
+        <li>React</li>
+        <li>Tailwind</li>
+        <li>Redux</li>
+      </ul>
+    </div>
+  )
+}
